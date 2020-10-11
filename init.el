bc82c72404849dc8da2b50fee65996634aa79080
laerling <laerling@posteo.de>
2020-10-11 21:34:38 +0200

    Add zenburn theme to packages

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 
 		       ;; themes
 		       spacemacs-theme
+		       zenburn-theme
 
 		       ))
 
@@ -184,7 +185,7 @@ If OTHER-WINDOW is non-nil, switch to the scratch buffer in other window."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit)))
+   '(zenburn-theme zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
