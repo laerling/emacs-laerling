f8628dc8732cf4a0925c7d54e5a182baf87ed6c7
laerling <laerling@posteo.de>
2020-10-12 09:59:00 +0200

    init.el: After adding melpa, refresh package list unconditionally

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (unless package--initialized (package-initialize t))
-  (unless package-archive-contents (package-refresh-contents))
+  (package-refresh-contents)
 
   ;; install packages
   (dolist (package package-list)
@@ -185,7 +185,8 @@ If OTHER-WINDOW is non-nil, switch to the scratch buffer in other window."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit)))
+   (quote
+    (zenburn-theme zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
