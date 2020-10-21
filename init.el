f39f8a1ff04467267d32e38d0bfda4ffa548792e
laerling <laerling@posteo.de>
2020-10-21 15:09:17 +0200

    Use rainbow delimiters

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 
 		       ;; UI
 		       magit
+		       rainbow-delimiters
 		       smart-window
 		       swiper
 		       zygospore
@@ -166,6 +167,9 @@ SET-BUFFER-MAJOR-MODE and insert INITIAL-SCRATCH-MESSAGE."
   (tool-bar-mode 0)
   (menu-bar-mode 0)
 
+  ;; use rainbow delimiters
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
   ;; show the column we're in
   (column-number-mode 1)
 
@@ -194,8 +198,7 @@ SET-BUFFER-MAJOR-MODE and insert INITIAL-SCRATCH-MESSAGE."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (zenburn-theme zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit))))
+   '(rainbow-delimiters zenburn-theme zygospore spacemacs-theme rust-mode nix-mode markdown-mode json-mode go-mode smart-window magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
