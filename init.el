da49cef94c7b3d7c5101440972a915f44a47af71
laerling <laerling@posteo.de>
2021-09-21 22:57:55 +0200

    C-s = isearch-forward, C-M-s = swiper
    Oftentimes I just want to search forward...

--- init.el
+++ init.el
@@ -147,7 +147,7 @@ SET-BUFFER-MAJOR-MODE and insert INITIAL-SCRATCH-MESSAGE."
   ;; package basics
   (ivy-mode 1)
   (which-key-mode 1)
-  (global-set-key (kbd "C-s") 'swiper)
+  (global-set-key (kbd "C-M-s") 'swiper)
 
   ;; buffers
   (global-set-key (kbd "M-n") 'next-buffer)
