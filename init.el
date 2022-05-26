cada404d581f6d4f3af0989ce038492fe4809d0b
laerling <laerling@posteo.de>
2022-05-26 13:59:22 +0200

    Map C-x M-p to list-processes

--- init.el
+++ init.el
@@ -175,6 +175,7 @@ consecutive space and/or tab characters."
   (global-set-key (kbd "C-x C-n") 'next-buffer)
   (global-set-key (kbd "C-x C-p") 'previous-buffer)
   (global-set-key (kbd "C-x s") 'switch-to-scratch-buffer)
+  (global-set-key (kbd "C-x M-p") 'list-processes)
   (global-set-key (kbd "C-x 4 s") (lambda () (interactive) (switch-to-scratch-buffer 'other-window))) ;; alternative to C-u C-x s
   (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
   (global-set-key (kbd "C-x C-r") 'revert-buffer)
