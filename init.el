e689aaf7b739d12ada5093fc317f4574bad7d3dc
laerling <laerling@posteo.de>
2022-10-18 21:23:29 +0200

    init.el: Map magit-status not only to C-x g but also C-x C-g

--- init.el
+++ init.el
@@ -214,6 +214,7 @@ consecutive space and/or tab characters."
 
   ;; git
   (global-set-key (kbd "C-x g") 'magit-status)
+  (global-set-key (kbd "C-x C-g") 'magit-status)
 
   ;; grepping
   (setq grep-command "grep --color -n -r ") ;; -n is needed for proper rendering in the *grep* buffer
