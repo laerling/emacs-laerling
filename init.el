b736ecc647c0f7f68233d8b8a6045828d53d317e
laerling <laerling@posteo.de>
2025-01-21 20:03:08 +0100

    Remove keybindings for functions that have a keybinding under C-x x
    - revert-buffer is not needed: revert-buffer-quick is on C-x x g
    
    - rename-buffer is on C-x x r

--- init.el
+++ init.el
@@ -194,8 +194,6 @@ consecutive space and/or tab characters."
   (global-set-key (kbd "C-x 4 s") (lambda () (interactive) (switch-to-scratch-buffer 'other-window))) ;; alternative to C-u C-x s
   (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
   (global-set-key (kbd "C-x C-1") 'zygospore-toggle-delete-other-windows)
-  (global-set-key (kbd "C-x C-r") 'revert-buffer)
-  (global-set-key (kbd "C-x M-r") 'rename-buffer)
   (global-set-key (kbd "C-x 6 0") 'kill-buffer-and-frame)
   (global-set-key (kbd "C-x C-k") 'kill-current-buffer)
   (global-set-key (kbd "C-<tab>") 'other-window)
